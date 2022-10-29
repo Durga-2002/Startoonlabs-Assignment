98419dc4b625e49083d7508269c3f3d16b02105a
Durga Vadige <you@example.com>
2022-10-29 10:18:17 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const progress = document.querySelector(".progressBar")
+const input = document.querySelector(".input")
+const maxInput = document.querySelector(".maxInput")
+let finalValue = 0
+let max = 0
+
+
+function changewidth() {
+    if (max > 0 && finalValue > max) {
+        alert(Error)
+        
+    }
+    else {
+        progress.style.width = `${(finalValue / max) * 100}%`
+    }
+
+}
+
+input.addEventListener("keyup", function () {
+    finalValue = parseInt(input.value, 10)
+    changewidth()
+})
+maxInput.addEventListener("keyup", function () {
+    max = parseInt(maxInput.value, 10)
+    changewidth()
+})
